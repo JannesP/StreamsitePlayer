e4f603c1695587301983f925a5033cb58bdcd671
JannesP <jannes.peters@live.de>
2015-08-22 16:56:20 +0200

    Initial commit. Added the raw form.

---
+++ Updater/Updater/FormMain.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Updater
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBarDownload = new System.Windows.Forms.ProgressBar();
+            this.labelDownloaded = new System.Windows.Forms.Label();
+            this.labelSpeed = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.richTextBoxChangelog = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // progressBarDownload
+            // 
+            this.progressBarDownload.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBarDownload.Location = new System.Drawing.Point(12, 28);
+            this.progressBarDownload.Name = "progressBarDownload";
+            this.progressBarDownload.Size = new System.Drawing.Size(486, 23);
+            this.progressBarDownload.TabIndex = 0;
+            // 
+            // labelDownloaded
+            // 
+            this.labelDownloaded.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelDownloaded.AutoSize = true;
+            this.labelDownloaded.Location = new System.Drawing.Point(423, 54);
+            this.labelDownloaded.Name = "labelDownloaded";
+            this.labelDownloaded.Size = new System.Drawing.Size(77, 13);
+            this.labelDownloaded.TabIndex = 1;
+            this.labelDownloaded.Text = "0000/0000 KB";
+            this.labelDownloaded.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // labelSpeed
+            // 
+            this.labelSpeed.AutoSize = true;
+            this.labelSpeed.Location = new System.Drawing.Point(12, 54);
+            this.labelSpeed.Name = "labelSpeed";
+            this.labelSpeed.Size = new System.Drawing.Size(58, 13);
+            this.labelSpeed.TabIndex = 2;
+            this.labelSpeed.Text = "0000 KB/s";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(12, 9);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(90, 13);
+            this.labelStatus.TabIndex = 3;
+            this.labelStatus.Text = "Downloading . . . ";
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.Location = new System.Drawing.Point(423, 265);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 4;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // richTextBoxChangelog
+            // 
+            this.richTextBoxChangelog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxChangelog.Enabled = false;
+            this.richTextBoxChangelog.Location = new System.Drawing.Point(12, 72);
+            this.richTextBoxChangelog.Name = "richTextBoxChangelog";
+            this.richTextBoxChangelog.Size = new System.Drawing.Size(405, 216);
+            this.richTextBoxChangelog.TabIndex = 5;
+            this.richTextBoxChangelog.Text = "";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 300);
+            this.Controls.Add(this.richTextBoxChangelog);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.labelSpeed);
+            this.Controls.Add(this.labelDownloaded);
+            this.Controls.Add(this.progressBarDownload);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(297, 204);
+            this.Name = "FormMain";
+            this.Text = "StreamsitePlayer Updater";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBarDownload;
+        private System.Windows.Forms.Label labelDownloaded;
+        private System.Windows.Forms.Label labelSpeed;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.RichTextBox richTextBoxChangelog;
+    }
+}
+
