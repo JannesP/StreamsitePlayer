74e584c61e09f815b70b7d588d343cd9284a00ba
JannesP <jannespeters.privat@gmail.com>
2015-10-30 22:28:37 +0100

    Make tempPlayer.html hidden.

--- StreamsitePlayer/StreamsitePlayer/Player/JwPlayer/JwPlayerControl.cs
+++ StreamsitePlayer/StreamsitePlayer/Player/JwPlayer/JwPlayerControl.cs
@@ -168,7 +168,14 @@ namespace StreamsitePlayer.JwPlayer
 
         private void DisplayHtml(string html)
         {
+            if (File.Exists(JW_TEMP_SITE_PATH))
+            {
+                FileInfo fi1 = new FileInfo(JW_TEMP_SITE_PATH);
+                fi1.Attributes &= ~FileAttributes.Hidden;
+            }
             File.WriteAllText(JW_TEMP_SITE_PATH, html);
+            FileInfo fi = new FileInfo(JW_TEMP_SITE_PATH);
+            fi.Attributes |= FileAttributes.Hidden;
             string path = Path.Combine("file:///" + Util.GetAppFolder(), JW_TEMP_SITE_PATH);
             base.Navigate(new Uri(path));
             while (base.ReadyState != WebBrowserReadyState.Complete)
