7beedb72b26548ba5b5a62d298db469b0a602f9a
JannesP <jannespeters.privat@gmail.com>
2016-12-17 10:07:14 +0100

    Change version to 0.7.2.1

--- StreamsitePlayer/Uninstaller/Properties/SharedAssemblyInfo.cs
+++ StreamsitePlayer/Uninstaller/Properties/SharedAssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SeriesPlayer")]
-[assembly: AssemblyCopyright("Copyright © 2015 Jannes Peters")]
+[assembly: AssemblyCopyright("Copyright © 2016 Jannes Peters")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.2.0")]
+[assembly: AssemblyVersion("0.7.2.1")]
 [assembly: NeutralResourcesLanguage("en-US")]
